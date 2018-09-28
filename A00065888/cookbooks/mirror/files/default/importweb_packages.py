4a6e392520fb199e262b2013ee2f23e5fc0857a4
nikoremi97 <nikoremi97@hotmail.com>
2018-09-28 15:50:29 -0500

    Update importeweb_packages python function

--- A00065888/cookbooks/mirror/files/default/importweb_packages.py
+++ A00065888/cookbooks/mirror/files/default/importweb_packages.py
@@ -1,6 +1,10 @@
 import urllib, json
-url = "https://raw.githubusercontent.com/nikoremi97/sd2018b-exam1/nrecalde/sd2018b-exam1/A00065888/cookbooks/mirror/files/default/packages.json "
+import os
+url = "https://raw.githubusercontent.com/nikoremi97/sd2018b-exam1/nrecalde/sd2018b-exam1/A00065888/cookbooks/mirror/files/default/packages.json"
 response = urllib.urlopen(url)
 data = json.loads(response.read())
-
-print data
+x=""
+for i in data:
+       x =x + " " + i["package"]
+print x
+os.system("yum install -y" +x)
