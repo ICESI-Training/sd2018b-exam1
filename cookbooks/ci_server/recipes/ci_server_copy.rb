28789283fdf503d41a9288a6434373972cd3c2e5
JonatanOrdonez <jonatan-ordonez@hotmail.com>
2018-10-01 15:08:54 -0500

    Update folder files

--- cookbooks/ci_server/recipes/ci_server_copy.rb
+++ cookbooks/ci_server/recipes/ci_server_copy.rb
@@ -1,13 +1,14 @@
 cookbook_file '/home/vagrant/endpoint/scripts/deploy.sh' do
-	source 'endpoint/scripts/deploy.sh'
+	source 'deploy.sh'
 	  owner 'root'
 	  group 'root'
 	  mode '0644'
 	  action :create
 end
 
+
 cookbook_file '/home/vagrant/endpoint/requirements.txt' do
-	source 'endpoint/requirements.txt'
+	source 'requirements.txt'
 	  owner 'root'
 	  group 'root'
 	  mode '0644'
@@ -15,15 +16,15 @@ cookbook_file '/home/vagrant/endpoint/requirements.txt' do
 end
 
 cookbook_file '/home/vagrant/endpoint/gm_analytics/handlers.py' do
-	source 'endpoint/gm_analytics/handlers.py'
+	source 'handlers.py'
 	  owner 'root'
 	  group 'root'
 	  mode '0644'
 	  action :create
 end
 
-cookbook_file '/home/vagrant/endpoint/gm_analytics/swagger/indexer.yml' do
-	source 'endpoint/gm_analytics/swagger/indexer.yaml'
+cookbook_file '/home/vagrant/endpoint/gm_analytics/swagger/indexer.yaml' do
+	source 'indexer.yaml'
 	  owner 'root'
 	  group 'root'
 	  mode '0644'
