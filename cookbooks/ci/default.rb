aef1a7fa3c44186570acace30ba5f1cdb9e83f00
yosoyafa <zehcort@hotmail.es>
2018-10-04 18:01:14 -0500

    Update ci-server and vagrant file

--- cookbooks/ci/default.rb
+++ cookbooks/ci/default.rb
@@ -1,6 +1,6 @@
-include_recipe 'install wget'
-include_recipe 'install_unzip'
-include_recipe 'ci_server::install_python3.6_and_tools'
-include_recipe 'ci_server::install_ngrok'
+include_recipe 'ci::install wget'
+include_recipe 'ci::install_unzip'
+include_recipe 'ci::install_python3.6_and_tools'
+include_recipe 'ci::install_ngrok'
 
 
