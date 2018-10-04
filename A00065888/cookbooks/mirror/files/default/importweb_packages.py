a46c4e2841f0cad913652203bbe36aaaafc95a97
nikoremi97 <nikoremi97@hotmail.com>
2018-10-04 10:16:15 -0500

    Update packages.json directory

--- A00065888/cookbooks/mirror/files/default/importweb_packages.py
+++ A00065888/cookbooks/mirror/files/default/importweb_packages.py
@@ -1,6 +1,6 @@
 import urllib, json
 import os
-url = "https://raw.githubusercontent.com/nikoremi97/sd2018b-exam1/nrecalde/sd2018b-exam1/A00065888/cookbooks/mirror/files/default/packages.json"
+url = "https://raw.githubusercontent.com/nikoremi97/sd2018b-exam1/nrecalde/sd2018b-exam1/A00065888/packages.json"
 response = urllib.urlopen(url)
 data = json.loads(response.read())
 x=""
