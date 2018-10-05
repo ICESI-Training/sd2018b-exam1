ddeeb91ae8ae57a224d34c19ee136e8e9462c111
Jorge Castaño <jorgeeliecercastao@outlook.com>
2018-10-05 11:13:03 -0500

    Update

---
+++ cookbooks/ci/files/default/flask_endpoint/gm_analytics/handlers.py
@@ -0,0 +1,27 @@
+import os
+import logging
+import requests
+import json
+import urllib
+from fabric import Connection
+from flask import request
+
+def repository_changed():
+    logging.debug('Event Received')
+    post_json_data = request.get_data()
+    string = str(post_json_data, 'utf-8')
+    jsonFile = json.loads(string)
+    pull_id = jsonFile["pull_request"]["head"]["sha"]
+    url = 'https://raw.githubusercontent.com/juliand7gj/sd2018b-exam1/' + pull_id + '/packages.json'
+    response = urllib.urlopen(url)
+    packages_json = json.loads(response.content)
+    install_packages = ""
+    for i in packages_json:
+        commands = i["commands"].split(";")
+        for j in commands:
+                install_packages = install_packages + " " + j
+    text = Connection('192.168.131.22').run('sudo yum install --downloadonly --downloaddir=/var/repo' + install_packages)
+    logging.debug(text)
+    result = {'command_return': 'Bien'}
+    return result
+
