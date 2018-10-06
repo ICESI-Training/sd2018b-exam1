40f1d423d35639d53cde2af80a4855ad03acda39
Sistemas Distribuidos <ana_fernanda_25@hotmail.com>
2018-10-06 18:15:22 -0500

    Sintaxis correction

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 **Student:** Ana Valderrama  
 **Student ID:** A00065868  
 **Email:** ana_fernanda_25@hotmail.com  
-  
+
 ### Goals
 * Perform in an autonomously way the automatic provisioning of an specific infrastructure.  
 * Diagnose and execute in an autonomously way all needed actions to achieve global stable infrastructures.  
@@ -21,7 +21,8 @@
 
 ### Description   
 
-To achieve the goals presented before is necessary to deploy a platform with the following requirements:  
-*   First, it was necessary to create a Vagrantfile with
-
-
+To achieve the goals presented before is necessary to deploy a platform based on four virtual machines, their principal functions are described below:  
+- **CentOS7 DHCP Server**  
+    * It's the one that must assign an IP address to clients of the subnet **192.168.137.0** in a range between **192.168.137.15** to **192.168.137.150**. The default gateway in this case was the same as the one at the university (**192.168.130.1**).  
+- **CentOS7 YUM Mirror Client**
+    * Depends on the **CentOS7 YUM Mirror Server** 
--- cookbooks/ciserver/files/default/endpoint/gm_analytics/handlers.py
+++ cookbooks/ciserver/files/default/endpoint/gm_analytics/handlers.py
@@ -8,14 +8,13 @@ def get_pullrequest_packages():
     string_json      = str(post_json_data, 'utf-8')
     json_pullrequest = json.loads(string_json)
     pullrequest_sha  = json_pullrequest["pull_request"]["head"]["sha"]
-    packages_url     = "https://raw.githubusercontent.com/anavalderrama25/sd2018b-exam1/"+pullrequest_sha+"packages.json"
+    packages_url     = 'https://raw.githubusercontent.com/anavalderrama25/sd2018b-exam1/'+pullrequest_sha+'/packages.json'
     response_packages_url = requests.get(packages_url)
     packages_data    =  json.loads(response_packages_url.content)
     packages_line = ""
     for i in packages_data:
-        packages = i[package_commands]
-        for j in packages:
-            packages_line = packages_line + " " + j
-    connect         = Connection('vagrant@192.168.137.3').run('sudo yum install --downloadonly --downloaddir=/var/repo' + packages_line)
+        packages = i['package_command']
+        packages_line = packages_line + packages
+    connect         = Connection('vagrant@192.168.137.3').run('sudo yum install --downloadonly --downloaddir=/var/repo ' + packages_line)
     out = {'command_return': 'OK'}
     return out
