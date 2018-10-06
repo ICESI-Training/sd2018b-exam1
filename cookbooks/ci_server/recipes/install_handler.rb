6f74b591ad47ffde547769baa381de69f7a4ad4b
zehcort <zehcort@hotmail.es>
2018-10-05 20:38:59 -0500

    Update mirror and ci

--- cookbooks/ci_server/recipes/install_handler.rb
+++ cookbooks/ci_server/recipes/install_handler.rb
@@ -1,15 +1,13 @@
 bash 'set_endpoint' do
 	  code <<-EOH
-	     mkdir /home/vagrant/request_handler
-	     cd /home/vagrant/request_handler
-	     mkdir scripts
-	     mkdir gm_analytics
-	     cd gm_analytics
+	     mkdir /home/vagrant/scripts
+	     mkdir /home/vagrant/gm_analytics
+	     cd /home/vagrant/gm_analytics
 	     mkdir swagger
   	  EOH
 end
 
-cookbook_file '/home/vagrant/request_handler/requirements.txt' do
+cookbook_file '/home/vagrant/requirements.txt' do
 	source 'endpoint/requirements.txt'
 	  owner 'root'
 	  group 'root'
@@ -17,7 +15,7 @@ cookbook_file '/home/vagrant/request_handler/requirements.txt' do
 	  action :create
 end
 
-cookbook_file '/home/vagrant/request_handler/scripts/deploy.sh' do
+cookbook_file '/home/vagrant/scripts/deploy.sh' do
 	source 'endpoint/scripts/deploy.sh'
 	  owner 'root'
 	  group 'root'
@@ -25,7 +23,7 @@ cookbook_file '/home/vagrant/request_handler/scripts/deploy.sh' do
 	  action :create
 end
 
-cookbook_file '/home/vagrant/request_handler/gm_analytics/handlers.py' do
+cookbook_file '/home/vagrant/gm_analytics/handlers.py' do
 	source 'endpoint/gm_analytics/handlers.py'
 	  owner 'root'
 	  group 'root'
@@ -33,7 +31,7 @@ cookbook_file '/home/vagrant/request_handler/gm_analytics/handlers.py' do
 	  action :create
 end
 
-cookbook_file '/home/vagrant/request_handler/gm_analytics/swagger/indexer.yaml' do
+cookbook_file '/home/vagrant/gm_analytics/swagger/indexer.yaml' do
 	source 'endpoint/gm_analytics/swagger/indexer.yaml'
 	  owner 'root'
 	  group 'root'
